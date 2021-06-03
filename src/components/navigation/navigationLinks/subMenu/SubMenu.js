6daffc4cea7df02e156967858bb8320b6fe98b14
Oleg Zablotskyi <Zablotsky@gmail.com>
2021-06-03 15:33:15 -0400

    changed subMenu

--- src/components/navigation/navigationLinks/subMenu/SubMenu.js
+++ src/components/navigation/navigationLinks/subMenu/SubMenu.js
@@ -4,6 +4,36 @@ import SubMenuLink from './subMenuLink/SubMenuLink';
 
 const SubMenu = (props) => {
 
+    const subMenuService = [{
+            name: 'Design',
+            link: '/service/design'
+        },
+        {
+            name: 'Roofing',
+            link: '/service/roofing'
+        },
+        {
+            name: 'Electrical',
+            link: '/service/Electrical'
+        },
+        {
+            name: 'Plumbing',
+            link: '/service/plumbing'
+        },
+        {
+            name: 'HVAC',
+            link: '/service/HVAC'
+        },
+        {
+            name: 'Painting',
+            link: '/service/painting'
+        },
+        {
+            name: 'Doors/Windows',
+            link: '/service/painting'
+        },
+    ]
+
     const handleOnMouseOver = (e) => {
         e.target.style.opacity = '1';
     }
@@ -12,22 +42,17 @@ const SubMenu = (props) => {
 
     return (
         <div className="nav-submenu-container" onMouseOver = {handleOnMouseOver}>
-            <SubMenuLink name="Design" link="/service/design"/>
-            <SubMenuLink name="Roofing" link="/service/doofing"/>
-            < SubMenuLink name = "Electrical"
-            link = "/service/painting" / >
-                <
-                SubMenuLink name = "Plumbing"
-            link = "/service/painting" / >
-                <
-                SubMenuLink name = "HVAC"
-            link = "/service/plumbing" / >
-                <
-                SubMenuLink name = "Painting"
-            link = "/service/plumbing" / >
-                <
-                SubMenuLink name = "DOORS/WINDOWS"
-            link = "/service/plumbing" / >
+            {
+                subMenuService.map(subMenu =>
+                        <
+                    SubMenuLink name = {
+                        subMenu.name
+                    }
+                    link = {
+                        subMenu.link
+                    }
+                    />)
+                    }
         </div>
     )
 }
