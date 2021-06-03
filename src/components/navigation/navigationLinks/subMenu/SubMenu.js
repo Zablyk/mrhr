8f092a51050b2e4d552ccb6919f3a8debbd3c31c
Zablyk <zablotsky@gmail.com>
2021-06-02 21:10:12 -0400

    added subMenu opacity style

--- src/components/navigation/navigationLinks/subMenu/SubMenu.js
+++ src/components/navigation/navigationLinks/subMenu/SubMenu.js
@@ -3,8 +3,15 @@ import './SubMenu.css';
 import SubMenuLink from './subMenuLink/SubMenuLink';
 
 const SubMenu = (props) => {
+
+    const handleOnMouseOver = (e) => {
+        e.target.style.opacity = '1';
+    }
+
+
+
     return (
-        <div className="nav-submenu-container">
+        <div className="nav-submenu-container" onMouseOver = {handleOnMouseOver}>
             <SubMenuLink name="Design" link="/service/design"/>
             <SubMenuLink name="Roofing" link="/service/doofing"/>
             <SubMenuLink name="Painting" link="/service/painting"/>
