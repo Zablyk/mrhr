07bbefe5bb3ed5f4b18019888a6458018fb3e434
Oleg Zablotskyi <Zablotsky@gmail.com>
2021-06-17 11:50:50 -0400

    added text in hero

--- src/components/header/hero/Hero.js
+++ src/components/header/hero/Hero.js
@@ -4,7 +4,12 @@ import './Hero.css';
 const Hero = () => {
     return (
         <div className="hero-container">
-            
+            <div>
+                <h3> Mr. Home Renovation </h3>
+                <h1> Let us Renovate or Construct </h1>
+                <h1>Your Dream in Life</h1>
+                <button>Contuct us</button>
+            </div>
         </div>
     )
 }
