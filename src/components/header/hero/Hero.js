f50f8a0d4f72a091097ea5b2da994f78cda63b2b
Zablyk <zablotsky@gmail.com>
2021-06-24 19:04:42 -0400

    hero text with animation

--- src/components/header/hero/Hero.js
+++ src/components/header/hero/Hero.js
@@ -5,10 +5,11 @@ const Hero = () => {
     return (
         <div className="hero-container">
             <div className="hero-container-text">
-                <h3> Mr. Home Renovation </h3>
-                <h1> Let us Renovate or Construct </h1>
+                <div className="hero-overlay"></div>
+                <h3>Mr. Home Renovation</h3>
+                <h1>Let us Renovate or Construct</h1>
                 <h1>Your Dream in Life</h1>
-                <button>Contuct us</button>
+                <button>Contact us</button>
             </div>
         </div>
     )
