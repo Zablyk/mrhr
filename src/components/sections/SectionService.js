7ba9b9b05e6fd6465535002448bd8cf70f771df3
Oleg Zablotskyi <Zablotsky@gmail.com>
2021-07-10 17:39:05 -0400

    section service

--- src/components/sections/SectionService.js
+++ src/components/sections/SectionService.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './SectionService.css';
 import ImageServiceMan from '../../assets/img/service-man.png'
+import ImageServiceDesign from '../../assets/img/service-1.jpg'
+import ImageServiceRoofing from '../../assets/img/service-2.jpg'
+import ImageServicePlumbing from '../../assets/img/service-3.jpg'
 
 const SectionService = () => {
     return (
         <div className = "section-service-content">
             <img src = {ImageServiceMan} alt="Service Man"/>
-            <h3>Our Services</h3>
-            <h2>Unique and Quality Service Makes Clients Happy</h2>
+            
+            <div className="section-service-content-text">
+                <h3>Our Services</h3>
+                <h2>Unique and Quality Service Makes Clients Happy</h2>
+                    <div className="section-service-content-img">
+                         <img src = {ImageServiceDesign} alt="Service Design"/>
+                         <img src = {ImageServiceRoofing} alt="Service Roffing"/>
+                         <img src = {ImageServicePlumbing} alt="Service Plumbing"/>
+                     </div>
+            </div>
+            
         </div>
     )
 }
